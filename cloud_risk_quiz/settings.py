5efd4a6dcd3da46e7c64fb41901d02633f17c7df
Aneesh114 <aneeshreddy114@gmail.com>
2022-11-01 21:18:50 +0530

    new

--- cloud_risk_quiz/settings.py
+++ cloud_risk_quiz/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -123,6 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#STATICFILES_DIRS=[BASE_DIR/'static']
+STATICFILES_DIRS=[BASE_DIR/'static']
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR,'staticfiles'))
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'))
+#STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'))
